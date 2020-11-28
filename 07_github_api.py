ad9b1e1425de11fcac722717be662e66bb8f0a6b
galileoguzman <galileoguzman@gmail.com>
2020-11-28 12:21:26 -0600

    Added function to get a github user using python requests

--- 07_github_api.py
+++ 07_github_api.py
@@ -3,5 +3,15 @@ import requests
 # CONSTANTS
 BASE_URL = 'https://api.github.com/'
 
-response = requests.get(BASE_URL)
-print(response.status_code)
+# FUNCTIONS
+def get_github_user(username):
+    url = f'{BASE_URL}users/{username}'
+    respose = requests.get(url)
+    if respose.status_code == 200:
+        return respose.json()
+    return None
+
+
+username = input('Give me a Github username:\t')
+user = get_github_user(username)
+print(user)
