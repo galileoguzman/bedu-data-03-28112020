717728043b65f9f5e1fb6922e158b1454b74d2f5
galileoguzman <galileoguzman@gmail.com>
2020-11-28 13:40:49 -0600

    Second approach for csv reading

---
+++ 10_lector_dos_csv.py
@@ -0,0 +1,12 @@
+# Utilizando modulo csv para lectura de archivos con extension csv
+import csv
+
+filename = 'employees.csv'
+
+with open(filename, mode='r') as csv_file:
+    csv_reader = csv.DictReader(csv_file)
+    for row in csv_reader:
+        # print(row['Salary'])
+        s = row.get('Salary')
+        n = row.get('Name')
+        print(f'{n} earns {s}')
