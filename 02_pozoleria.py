a1cdfba4f5e35831492fe34f7d2fd3f85f2ab95d
galileoguzman <galileoguzman@gmail.com>
2020-11-28 10:22:15 -0600

    Improvements on pozoleria

--- 02_pozoleria.py
+++ 02_pozoleria.py
@@ -1,7 +1,9 @@
 IVA = 0.16
 
-precios_con_iva = [415, 90, 355, 385, 115, 100, 250, 660]
+precios_sin_iva = [415, 90, 355, 385, 115, 100, 250, 660]
 
-for precio in precios_con_iva:
-    resultado = precio * (1 + IVA)
-    print(f'${precio} con IVA incluido: $', round(resultado, 2))
+for precio in precios_sin_iva:
+    resultado = precio * (1 + IVA) # 104.48726378623
+    resultado = round(resultado, 2) # 104.4
+    resultado_impreso = f'${precio} con IVA incluido: ${resultado}'
+    print(resultado_impreso)
