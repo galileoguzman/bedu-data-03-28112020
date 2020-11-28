f2096c2375135701a52a5d1a47ba248006d07b2f
galileoguzman <galileoguzman@gmail.com>
2020-11-28 13:29:50 -0600

    Add first approach for csv reading

---
+++ 09_lector_csv.py
@@ -0,0 +1,9 @@
+# Utilizando modulo csv para lectura de archivos con extension csv
+import csv
+
+filename = 'employees.csv'
+
+with open(filename) as csv_file:
+    csv_reader = csv.reader(csv_file)
+    for row in csv_reader:
+        print(row)
