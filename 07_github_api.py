51933dc1d09439ab8fa86813a2d559c171fcc361
galileoguzman <galileoguzman@gmail.com>
2020-11-28 12:42:34 -0600

    Added function to download a image

--- 07_github_api.py
+++ 07_github_api.py
@@ -12,6 +12,19 @@ def get_github_user(username):
     return None
 
 
+def download_github_user_avatar(avatar_url, username):
+    response = requests.get(avatar_url)
+    if response.status_code == 200:
+        # download a file from internet
+        response_content = response.content
+        filename = f'tmp/{username}.png'
+        with open(filename, 'wb') as image:
+            image.write(response_content)
+            return filename
+    return None
+
+
 username = input('Give me a Github username:\t')
 user = get_github_user(username)
-print(user)
+filename = download_github_user_avatar(user['avatar_url'], username)
+print(filename)
